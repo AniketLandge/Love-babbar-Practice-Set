843999969bd5687af77bdb56a2a99e350937e31f
AniketLandge <aniketlandge8896@gmail.com>
2023-05-03 17:23:40 +0530

    added code for bit manipulation

--- Bit Manipulation/countsetbits.js
+++ Bit Manipulation/countsetbits.js
@@ -1,1 +1,20 @@
 //Count set bits in an integer
+
+//5 
+// 101 => make & by 1 
+//101
+//001 = 001
+
+
+let setBit = (n) => {
+    let count = 0;
+    while(n) {
+        let bit =  n&1;
+        if(bit) count++
+        n = n>>1 
+    }
+    return count;
+}
+
+
+console.log(setBit(15)); 
